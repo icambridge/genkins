0cbe2cf05315e6698c7e8ff5c4460146c8742663
Iain Cambridge <github@icambridge.me>
2014-03-23 21:13:32 +0000

    Add trigger build with parameters

--- src/genkins/builds.go
+++ src/genkins/builds.go
@@ -2,6 +2,7 @@ package genkins
 
 import (
 	"fmt"
+	"net/url"
 )
 
 type BuildsService struct {
@@ -27,6 +28,30 @@ func (s BuildsService) Trigger(job string) error {
 	return nil
 }
 
+func (s BuildsService) TriggerWithParameters(job string, parameters map[string]string) error {
+
+	v := url.Values{}
+
+	for key, value := range parameters {
+		v.Add(key, value)
+	}
+
+	url := fmt.Sprintf("/job/%s/buildWithParameters?%s", job, v.Encode())
+
+	req, err := s.client.NewRequest("POST", url, nil)
+
+	if err != nil {
+		return err
+	}
+
+	err = s.client.Do(req, nil)
+
+	if err != nil {
+		return err
+	}
+	// Could do return err, but this seems clearer.
+	return nil
+}
 
 type Hook struct {
 	Name  string `json:"name"`
--- src/genkins/builds_test.go
+++ src/genkins/builds_test.go
@@ -28,3 +28,32 @@ func TestBuildsService_Trigger(t *testing.T) {
 		t.Error("Didn't hit api")
 	}
 }
+func TestBuildsService_TriggerWithParameters(t *testing.T) {
+	setUp()
+	defer tearDown()
+	hitApi := false
+	mux.HandleFunc("/job/test/buildWithParameters", func(w http.ResponseWriter, r *http.Request) {
+			if m := "POST"; m != r.Method {
+				t.Errorf("Request method = %v, expected %v", r.Method, m)
+			}
+			if qs:= "testone"; qs != r.URL.Query().Get("parameter") {
+				t.Errorf("Query string tree = %v, expected %v", r.URL.Query().Get("parameter"), qs)
+			}
+			hitApi = true
+			fmt.Fprint(w, `{"status":"success"}`)
+		})
+
+	m := map[string]string{
+		"parameter": "testone",
+	}
+
+	err := client.Builds.TriggerWithParameters("test", m)
+
+	if err != nil {
+		t.Errorf("Didn't expect an error got : %v", err)
+	}
+
+	if hitApi == false {
+		t.Error("Didn't hit api")
+	}
+}
